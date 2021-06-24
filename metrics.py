379c9c884491982bc206fca7c8c4aec6bfeea462
Jorge Mederos Alvarado <jmederosalvarado@gmail.com>
2021-06-24 18:08:28 -0400

    commit

--- metrics.py
+++ metrics.py
@@ -1,5 +1,16 @@
+import math
 import numpy as np
 import cv2.cv2 as cv2
+
+
+def calculate_psnr(img1, img2):
+    # img1 and img2 have range [0, 255]
+    img1 = img1.astype(np.float64)
+    img2 = img2.astype(np.float64)
+    mse = np.mean((img1 - img2) ** 2)
+    if mse == 0:
+        return float("inf")
+    return 20 * math.log10(255.0 / math.sqrt(mse))
 
 
 def ssim(img1, img2):
