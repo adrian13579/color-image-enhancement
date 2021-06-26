2b173aad0bd11a71e1537bd088a20f4a345e2258
Jorge Mederos Alvarado <jmederosalvarado@gmail.com>
2021-06-26 01:09:40 -0400

    commit

--- st_main.py
+++ st_main.py
@@ -1,8 +1,7 @@
 import numpy as np
+import pywt
 import streamlit as st
 from PIL import Image
-
-import pywt
 
 import metrics
 from color import enhance_image
@@ -17,19 +16,31 @@ def fix_channels(array):
 def get_opened_image(image_file):
     return fix_channels(np.array(Image.open(image_file)))
 
+@st.cache(allow_output_mutation=True)
+def get_upload_status():
+    return {"image": None}
+
+def upload_image():
+    image_dict=get_upload_status()
+    st.sidebar.subheader("Load Image")
+    image = st.sidebar.file_uploader("Upload an image", type="jpg")
+    image_dict.update({"image":image})
 
 def main():
-    st.sidebar.subheader("Load Image")
-    image_file = st.sidebar.file_uploader("Upload an image", type="png")
-    button = st.sidebar.button("Load")
+    upload_image()
+    image_dict = get_upload_status()
+    image_file = image_dict["image"]
+
+    if image_file is None:
+        return
 
     st.sidebar.subheader("Parameter Tunning")
     alpha = st.sidebar.slider("alpha", min_value=0.0, max_value=1.0, step=0.01)
     wj = st.sidebar.slider("wj", min_value=0, max_value=8)
     K = st.sidebar.slider("K", min_value=0, max_value=80)
-    wavl = st.sidebar.selectbox('Select a wavelet', pywt.wavelist())
+    wavl = st.sidebar.selectbox("Select a wavelet", pywt.wavelist())
 
-    if image_file and button:
+    if image_file:
         image = get_opened_image(image_file)
         with st.beta_expander("Selected Image", expanded=True):
             st.image(image, use_column_width=True)
@@ -38,10 +49,8 @@ def main():
         processed_image = enhance_image(image, alpha, wj, K, wavl)
         st.image(processed_image)
         st.markdown(f"## Metrics results:")
-        st.markdown(
-            f"### PSNR: **{metrics.calculate_psnr(image, processed_image)}**")
-        st.markdown(
-            f"### SSIM: **{metrics.calculate_ssim(image, processed_image)}**")
+        st.markdown(f"### PSNR: **{metrics.calculate_psnr(image, processed_image)}**")
+        st.markdown(f"### SSIM: **{metrics.calculate_ssim(image, processed_image)}**")
     else:
         st.markdown(f"# First load an image")
 
